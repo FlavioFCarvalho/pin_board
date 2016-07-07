c6010bf892583c9dd3f4cb12806640cd611fcd1c
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-07-07 17:20:44 -0300

    Criando a show Pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,11 @@
 class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy]
 	def index
-		
+		@pins = Pin.all.order("created_at DESC")
+	end
+
+	def show
+
 	end
 
 	def new
@@ -21,5 +26,9 @@ class PinsController < ApplicationController
 
 	def pin_params
 		params.require(:pin).permit(:title, :description)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
 	end
 end
